8542d1356ec0e5a0b4928eac29691d5cdb82f27f
shreya231008 <shreyachauhan2308@gmail.com>
2020-08-11 12:59:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import sub
+import multiply
+import Add
+import div
+print("calculator")
+print("1.Add \n2.Division \n3.Multiply \n4.Subtract")
+
+
+def switch():
+
+    a = int(input("enter first number"))
+    b = int(input("enter second number"))
+    option = int(input("choose the desired operation "))
+    if option == 1:
+       print(Add.add(a,b))
+    if option == 2:
+        print(div.divide(a,b))
+    if option == 3:
+        print(multiply.multiply(a, b))
+    if option == 4:
+        print(sub.sub(a, b))
+while(True):
+ switch()
